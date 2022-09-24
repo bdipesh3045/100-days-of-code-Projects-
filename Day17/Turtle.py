26d3bbc6a4ac37f75f68da47e062a82f100120d7
Dipesh_codes <111185281+bdipesh3045@users.noreply.github.com>
2022-09-24 13:13:30 -0400

    Create Turtle.py

---
+++ Day17/Turtle.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.forward(100)
+timmy.color("Red")
+my_screen= Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
